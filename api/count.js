9badebfa2121b204467b7ced94e6b977a4f43cb0
Ilmir Shaikhutdinov <ilmirw@gmail.com>
2023-06-21 11:42:43 +0300

    use commonjs and js files in api

--- api/count.js
+++ api/count.js
@@ -1,3 +1,4 @@
+/*
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 import sitesJson from '../data/sites.json' assert { type: 'json' };
@@ -19,3 +20,21 @@ export default async function handler(
     });
   }
 }
+*/
+
+const sitesJson = require('../data/sites.json');
+
+const sites = sitesJson.sites;
+
+module.exports = async (request, response) => {
+  try {
+    response.status(200).json({
+      count: sites.length
+    });
+  } catch (err) {
+    response.status(500).json({
+      success: false,
+      error: err.message || err
+    });
+  }
+};
